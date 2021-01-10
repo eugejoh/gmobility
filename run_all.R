65f28ac89e337ee7f58231701bc27818c3a5ac56
eugejoh <ejoh@bu.edu>
2021-01-10 15:06:05 -0500

    source script

---
+++ run_all.R
@@ -0,0 +1,18 @@
+# Run All -----------------------------------------------------------------
+library(here)
+library(readr)
+library(dplyr)
+library(purrr)
+library(RSQLite)
+library(DBI)
+library(rmarkdown)
+
+# update DB
+source(here::here("00_update_db.r"))
+
+# create heatmaps
+source(here::here("plot_heatmaps.R"))
+max_date <- max(prov$date, na.rm = TRUE)
+
+# update .rmd
+render_site(here::here("_site"))
